778f109384056224e736877c608fbfacbe8f75a3
Adam Crockett <adam.crockett@dyson.com>
2019-11-19 21:06:57 +0000

    firther deletes to remove overengineered solutions or things that have came across from my vue boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-import './store';
 import AsyncComputed from "vue-async-computed";
-import router from "./routes";
 import Vue from "vue";
-import VueRouter from "vue-router";
+import BasicView from "./views/BasicView";
 // Styles
 import "./style/App.scss";
 // Components
 import Navigation from "./components/Navigation";
 
 Vue.use(AsyncComputed);
-Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
 export default new Vue({
-  name: 'Adam',
+  name: 'Listy',
   el: "#app",
   render() {
     return (
       <div class='app-Root'>
         <Navigation></Navigation>
-        <router-view></router-view>
+        <BasicView></BasicView>
       </div>
     )
   },
-  router,
 });
